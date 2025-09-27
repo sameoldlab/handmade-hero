306f6ac2cf2e07b7dff53851ba07aeb7c57567ed
ibro <sameoldlab@proton.me>
2025-09-27 13:16:42 +0100

    day 1

--- main.c
+++ main.c
@@ -1,59 +1,46 @@
-#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_messagebox.h>
+#include <stdio.h>
+#define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-/* We will use this renderer to draw into this window every frame. */
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
-/* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-  SDL_SetAppMetadata("Example Renderer Clear", "1.0",
-                     "com.example.renderer-clear");
+  SDL_SetAppMetadata("", "1.0", "supply.same.handmade");
 
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
 
-  if (!SDL_CreateWindowAndRenderer("examples/renderer/clear", 640, 480, 0,
-                                   &window, &renderer)) {
-    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-    return SDL_APP_FAILURE;
-  }
+  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Handmade Hero",
+                           "This is handmade hero", 0);
 
-  return SDL_APP_CONTINUE; /* carry on with the program! */
+  return SDL_APP_SUCCESS;
 }
 
-/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
   if (event->type == SDL_EVENT_QUIT) {
-    return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+    return SDL_APP_SUCCESS;
   }
-  return SDL_APP_CONTINUE; /* carry on with the program! */
+  // printf("%d", event->);
+  return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-  const double now = ((double)SDL_GetTicks()) /
-                     1000.0; /* convert from milliseconds to seconds. */
-  /* choose the color for the frame we will draw. The sine wave trick makes it
-   * fade between colors smoothly. */
+  const double now = ((double)SDL_GetTicks()) / 1000.0;
   const float red = (float)(0.5 + 0.5 * SDL_sin(now));
   const float green = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 2 / 3));
   const float blue = (float)(0.5 + 0.5 * SDL_sin(now + SDL_PI_D * 4 / 3));
-  SDL_SetRenderDrawColorFloat(
-      renderer, red, green, blue,
-      SDL_ALPHA_OPAQUE_FLOAT); /* new color, full alpha. */
+  SDL_SetRenderDrawColorFloat(renderer, red, green, blue,
+                              SDL_ALPHA_OPAQUE_FLOAT);
 
-  /* clear the window to the draw color. */
   SDL_RenderClear(renderer);
-
-  // put the newly-cleared rendering on the screen.
   SDL_RenderPresent(renderer);
-
-  // carry on with the program! 
-  return SDL_APP_CONTINUE; 
+  return SDL_APP_CONTINUE;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) { }
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {}
